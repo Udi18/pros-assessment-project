d9f96c403989d39562481d35afdb74a330ff1254
Udi Levy <webdevudi@gmail.com>
2019-01-26 16:53:02 -0600

    Updates

---
+++ precache-manifest.01b0d8003daec654382416048243cc3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c7f87b4f901e57b39f8",
+    "url": "/Udi18/pros-assessment-project/static/js/runtime~main.5c7f87b4.js"
+  },
+  {
+    "revision": "233d818fbbce2e6f9f69",
+    "url": "/Udi18/pros-assessment-project/static/js/main.233d818f.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/Udi18/pros-assessment-project/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "233d818fbbce2e6f9f69",
+    "url": "/Udi18/pros-assessment-project/static/css/main.d924279c.chunk.css"
+  },
+  {
+    "revision": "e28da31348d8d4478a8a4cc044525172",
+    "url": "/Udi18/pros-assessment-project/index.html"
+  }
+];
